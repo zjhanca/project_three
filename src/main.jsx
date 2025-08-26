e088655953d46b53fa3ec07e8abc5eb68417a2c6
Jhanca Berrio <jhanca123codm@gmail.com>
2025-08-26 11:08:45 -0500

    Actualización del proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'; // Asegúrate de que el archivo App.jsx esté en la misma carpeta
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.render(<App />, document.getElementById('root'));
